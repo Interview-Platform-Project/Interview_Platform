09b0e5868aebb4f726c1cfa7c5eddfe9402c4bf1
apromitXiaomi <realapromit@gmail.com>
2026-07-24 14:09:34 +0300

    Feat/Database architecture

--- apps/backend/prisma/migrations/20260715192829_test/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "userRole" AS ENUM ('USER', 'ADMIN', 'MODERATOR', 'SUPERADMIN');
-
--- CreateEnum
-CREATE TYPE "userStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'SUSPENDED', 'DELETED', 'BLOCKED');
